e0b0b4b565bc8c97c355ef9cda6d5a5e47117e08
David L. Inman <davidlevere@gmail.com>
2016-06-07 22:50:20 -0400

    incremental progress

--- parse.fs
+++ parse.fs
@@ -13,6 +13,23 @@ type Parser<'S, 'A> = ParseSource<'S> -> ParseResult<'S, 'A>
 
 // bind : ma -> (a -> mb) -> mb
 // mu : a -> ma
-(*type ParserBuilder() =
-    member this.Bind p g =
-    member this.Return a = *)
+type ParserBuilder() =
+    member this.Bind<'S, 'A> (p : Parser<'S, 'A>)  g = fun (s : ParseSource<'S>) -> 
+        match p s with 
+        | Success( value, source ) -> g value
+        | Failure source -> Failure source
+            
+    member this.Return a = fun source -> Success( a, source )
+
+let parse = new ParserBuilder()
+
+let getChar ( s : ParseSource<char array> ) = 
+    let (index, array) = s in
+        if array.[index] = 'a' then Success( 'a', (index, array) )
+        else Failure( index, array )
+
+(*let z : Parser<char array, char * char> = 
+    parse { let! a = getChar
+            let! b = getChar
+            return (a, b) }*)
+
