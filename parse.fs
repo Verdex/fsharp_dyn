8cd4724b6c677f1cb4150d72d3bcb02a3bd17c34
David L. Inman <davidlevere@gmail.com>
2016-06-07 22:02:52 -0400

    parser type def

--- parse.fs
+++ parse.fs
@@ -2,4 +2,17 @@
 
 module Parse
 
-let x = 0
+
+type ParseSource<'a> = int * 'a
+
+type ParseResult<'s, 'a>  =
+    | Success of 'a * ParseSource<'s>
+    | Failure
+
+type Parser<'s, 'a> = ParseSource<'s> -> ParseResult<'s, 'a>
+
+// bind : ma -> (a -> mb) -> mb
+// mu : a -> ma
+(*type ParserBuilder() =
+    member this.Bind p g =
+    member this.Return a = *)
