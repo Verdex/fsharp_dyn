74376d328e6a71b50fcb6d9dee4e43dce5eb6fe5
David L. Inman <davidlevere@gmail.com>
2016-06-09 21:53:26 -0400

    incremental progress

--- parse.fs
+++ parse.fs
@@ -9,27 +9,31 @@ type ParseResult<'S, 'A> =
     | Success of 'A * ParseSource<'S>
     | Failure of ParseSource<'S> 
 
-type Parser<'S, 'A> = ParseSource<'S> -> ParseResult<'S, 'A>
+type Parser<'S, 'A> = Parser of ( ParseSource<'S> -> ParseResult<'S, 'A> )
+
+let parseWith (Parser f) v = f v
 
 // bind : ma -> (a -> mb) -> mb
 // mu : a -> ma
 type ParserBuilder() =
-    member this.Bind<'S, 'A> (p : Parser<'S, 'A>)  g = fun (s : ParseSource<'S>) -> 
-        match p s with 
-        | Success( value, source ) -> g value
-        | Failure source -> Failure source
+    member this.Bind<'S, 'A, 'B> (p : Parser<'S, 'A>) (g : 'A -> Parser<'S, 'B>) = 
+        Parser( fun (s : ParseSource<'S>) -> 
+            match parseWith p s with 
+            | Success( value, source ) -> parseWith (g value) source
+            | Failure source -> Failure source )
             
-    member this.Return a = fun source -> Success( a, source )
+    member this.Return a = Parser( fun source -> Success( a, source ) )
 
 let parse = new ParserBuilder()
 
-let getChar ( s : ParseSource<char array> ) = 
+let getChar : Parser<char array, char> = Parser( fun s ->
     let (index, array) = s in
         if array.[index] = 'a' then Success( 'a', (index, array) )
-        else Failure( index, array )
+        else Failure( index, array ) )
 
-(*let z : Parser<char array, char * char> = 
-    parse { let! a = getChar
-            let! b = getChar
-            return (a, b) }*)
+let z (*: Parser<char array, char * char>*) = 
+    parse { let! a = getChar 
+            let! b = getChar 
+            return (a, b) 
+          }
 
